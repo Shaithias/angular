a5e24221d686767439863d3ca6bfd92a32207383
Shaithias <j_be_nimble@hotmail.com>
2017-09-26 15:54:41 -0500

    Carousel display of micro projects

---
+++ main.js
@@ -0,0 +1,57 @@
+var app=angular.module("carousel_app",[]);
+
+app.controller('carousel_controller',function($scope,$http)
+{
+	var urls=
+	[
+	'view1/view1.html',
+	'view2/view2.html',
+	'https://www.guru99.com/angularjs-tutorial.html',
+	'https://www.angularminds.com/blog/article/comparison-difference-between-angular1-vs-angular2-vs-angular4.html',
+	'https://justintimecoder.com/first-steps-with-ng-4-material-flex-layout-and-cli-1-3-build-optimiser/'
+
+
+	];
+	
+	var id='1';
+	var index=0;
+	
+	var iframe= document.createElement('iframe');
+	iframe.setAttribute('id',id);
+	iframe.setAttribute('src',urls[index]);
+	iframe.setAttribute('width',"100%");
+	iframe.setAttribute('height',"90%");
+
+	document.body.insertBefore(iframe,document.body.children[0]);
+	
+	$scope.left=function()
+	{
+		if(index<=0)
+		{
+			console.log(index);
+			index=urls.length-1;
+			iframe.setAttribute('src',urls[index]);
+		}
+		else
+		{
+			console.log(index);
+			index--;
+			iframe.setAttribute('src',urls[index]);
+		}
+	}
+	$scope.right=function()
+	{
+		if(index>=urls.length-1)
+		{
+			console.log(index);
+			index=0;
+			iframe.setAttribute('src',urls[index]);
+		}
+		else
+		{
+			console.log(index);
+			index++;
+			iframe.setAttribute('src',urls[index]);
+		}
+	}
+});
